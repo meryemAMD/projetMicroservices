769cd277755ddad8b472456611a4b4d591c436e9
LailaZou <laila.zouaq@gmail.com>
2019-12-21 17:28:34 +0100

    ajout de verification et activation de compte par mail

--- service-authentification/src/main/java/org/sid/dao/AppUserRepository.java
+++ service-authentification/src/main/java/org/sid/dao/AppUserRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
+
 @RepositoryRestResource
 public interface AppUserRepository extends MongoRepository<AppUser,String> {
     public AppUser findByUsername(String username);
+    public AppUser findByConfirmationToken(String confirmationToken);
+
 }
